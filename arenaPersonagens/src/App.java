9169c84d0a66805fe363a6c4caf70072fbc02f9e
roberta-parcianello <roberta.parcianello@docente.pr.senac.br>
2026-09-02 20:48:22 -0300

    pixel art

---
+++ arenaPersonagens/src/App.java
@@ -0,0 +1,70 @@
+import java.util.Scanner;
+
+public class App {
+    private static Scanner teclado = new Scanner(System.in);
+    public static void main(String[] args) throws Exception {
+        //exemplo menu
+        int op = 0;
+        do{
+            System.out.println("==========================================");
+            System.out.println("=     ⚔    ARENA DE PERSONAGENS    ⚔    =");
+            System.out.println("==========================================");
+            System.out.println("=                MENU                    =");
+            System.out.println("==========================================");
+            System.out.println(" 1 - Cadastrar Personagem");
+            System.out.println(" 2 - Listar Personagens");
+            System.out.println(" 3 - Executar Ataque");
+            System.out.println(" 4 - Usar Habilidade");
+            System.out.println(" 0 - Sair");
+            System.out.print("Opção: ");
+            op = teclado.nextInt();
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            System.out.println("░░░░░░░░░░░░░░▄▄▄▄▄░░░░░░░░░░░░░░");
+            System.out.println("░░░░░░░░░░▄██████████▄▄░░░░░░░░░░");
+            System.out.println("░░░░░░░░▄██████░█░██████▄░░░░░░░░");
+            System.out.println("░░░░░░▄████████░█░████████░░░░░░░");
+            System.out.println("░░░░░░█████████░█░█████████░░░░░░");
+            System.out.println("░░░░░██████████████████████░░░░░░");
+            System.out.println("░░░░░██████████████████████▄░░░░░");
+            System.out.println("░░░░▄████░░░░░▀█▄█▀░░░░░████░░░░░");
+            System.out.println("░░░▄█████░░░░░░█▄█░░░░░░█████░░░░");
+            System.out.println("░░▄████████▄▄▄█████▄▄▄████████░░░");
+            System.out.println("░▄████▀███████████████████▀████░░");
+            System.out.println("░████▀██████████░██████████▀████░");
+            System.out.println("████▀██████████░█░█▀████████▀████");
+            System.out.println("███▀▀░░░▀▀█▀█░█░█░█░█▀█▀▀░░░▀▀███");
+            System.out.println("░▀░░░░░░░░░░█░█░█░█░█░░░░░░░░░░▀░");
+            System.out.println("░░░░░░░░░░░░▀███████▀░░░░░░░░░░░░");
+            System.out.println("░░░░░░░░░░░░░░▀▀█▀▀░░░░░░░░░░░░░");
+
+
+
+            switch (op) {
+                case 1:
+                    
+                    break;
+            
+                default:
+                    break;
+            }
+
+        }while (op !=0);
+    }
+}
