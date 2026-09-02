212322cf238a7d93363162b6b795bc83b102a2a0
roberta-parcianello <roberta.parcianello@docente.pr.senac.br>
2026-09-02 20:03:57 -0300

    Integer cache

---
+++ curiosidadesJava/src/App.java
@@ -0,0 +1,18 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Integer n1 = 100;
+        Integer n2 = 100;
+
+        if (n1==n2){
+            System.out.println(n1+ "=" +n2);
+        }else System.out.println(n1 +" != "+n2);
+
+        Integer n3 = 1500;
+        Integer n4 = 1500;
+
+        if (n3==n4){
+            System.out.println(n3+ "=" +n4);
+        }else System.out.println(n3 +" != "+n4);
+
+    }
+}
